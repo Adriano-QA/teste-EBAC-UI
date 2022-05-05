3966ed58fd99111336819768fa01ecb3857ef89e
Adriano Aluizi <adrianoaluizi@hotmail.com>
2022-05-05 17:02:29 -0300

    Utilização de Listas e Variaveis

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -9,13 +9,17 @@ describe('Funcionalidade pré cadastro', () => {
     });
 
     it('Deve completar o pré cadastro com sucesso', () => {
-        cy.get('#reg_email').type(faker.internet.email())
+        let emailFaker = faker.internet.email(firstName);
+        let firstName = faker.name.firstName();
+        let lastName = faker.name.lastName();
+
+        cy.get('#reg_email').type(emailFaker)
         cy.get('#reg_password').type('teste@123')
         cy.get(':nth-child(4) > .button').click()
 
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
-        cy.get('#account_first_name').type(faker.name.firstName())
-        cy.get('#account_last_name').type(faker.name.lastName())
+        cy.get('#account_first_name').type(firstName)
+        cy.get('#account_last_name').type(lastName)
         cy.get('.woocommerce-Button').click()
 
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
