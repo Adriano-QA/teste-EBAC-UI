34c763dc7d8f24057b9c3b6844becc773a3b2e84
Adriano Aluizi <adrianoaluizi@hotmail.com>
2022-05-09 11:18:51 -0300

    Utilização de Custom Commands

--- cypress/integration/produtos.spec.js
+++ cypress/integration/produtos.spec.js
@@ -21,11 +21,17 @@ describe('Funcionalidade pagina de produtos', () => {
         cy.get('[class="product-block grid"]')
             .contains('Argus All-Weather Tank')
             .click()
-            cy.get('.button-variable-item-S').click()
-            cy.get(':nth-child(2) > .value > .variable-items-wrapper > .variable-item').click()
-            cy.get('.input-text').clear().type(quantidade)
-            cy.get('.single_add_to_cart_button').click()
-            cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
-            cy.get('.woocommerce-message').should('contain', quantidade + ' × “Argus All-Weather Tank” foram adicionados no seu carrinho.')
+        cy.get('.button-variable-item-S').click()
+        cy.get(':nth-child(2) > .value > .variable-items-wrapper > .variable-item').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Argus All-Weather Tank” foram adicionados no seu carrinho.')
+    });
+
+    it.only('Deve adicionar um produto ao carrinho com CUSTOM COMMANDS', () => {
+
+        cy.addProdutos('Argus All-Weather Tank', 'M', 'Black', 3)
+
     });
 });
